91577efcec73080314453835c0e106ce6c4d07d5
edenmorris <edenmorris123@gmail.com>
2023-03-05 21:14:37 +0000

    rollbar

--- backend-flask/app.py
+++ backend-flask/app.py
@@ -8,6 +8,11 @@ import watchtower
 import logging
 from time import strftime
 
+# Rollbar
+import rollbar
+import rollbar.contrib.flask
+from flask import got_request_exception
+
 from services.home_activities import *
 from services.notifications_activities import *
 from services.user_activities import *
@@ -64,6 +69,24 @@ XRayMiddleware(app, xray_recorder)
 FlaskInstrumentor().instrument_app(app)
 RequestsInstrumentor().instrument()
 
+# Rollbar -------
+rollbar_access_token = os.getenv('ROLLBAR_ACCESS_TOKEN')
+@app.before_first_request
+def init_rollbar():
+    """init rollbar module"""
+    rollbar.init(
+        # access token
+        rollbar_access_token,
+        # environment name
+        'production',
+        # server root directory, makes tracebacks prettier
+        root=os.path.dirname(os.path.realpath(__file__)),
+        # flask already sets up logging
+        allow_logging_basic_config=False)
+
+    # send exceptions from `app` to rollbar, using flask's signal system.
+    got_request_exception.connect(rollbar.contrib.flask.report_exception, app)
+
 frontend = os.getenv('FRONTEND_URL')
 backend = os.getenv('BACKEND_URL')
 origins = [frontend, backend]
@@ -89,6 +112,11 @@ def data_message_groups():
     return model['errors'], 422
   else:
     return model['data'], 200
+
+@app.route('/rollbar/test')
+def rollbar_test():
+    rollbar.report_message('Hello World!', 'warning')
+    return "Hello World!"
 
 @app.route("/api/messages/@<string:handle>", methods=['GET'])
 def data_messages(handle):
