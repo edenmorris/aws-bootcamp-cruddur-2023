e87a868c66dbed21d9d6b01c0ea1de75dbed2ae2
edenmorris <edenmorris123@gmail.com>
2023-03-25 18:12:18 +0000

    mock api interaction

--- backend-flask/services/home_activities.py
+++ backend-flask/services/home_activities.py
@@ -2,6 +2,8 @@ from datetime import datetime, timedelta, timezone
 from opentelemetry import trace
 import logging
 
+from lib.db import pool, query_wrap_array
+
 tracer = trace.get_tracer("home.activities")
 
 class HomeActivities:
@@ -61,6 +63,31 @@ class HomeActivities:
         'replies': []
         }
         results.insert(0, extra_crud)
+
+      sql = query_wrap_array("""
+      SELECT
+        activities.uuid,
+        users.display_name,
+        users.handle,
+        activities.message,
+        activities.replies_count,
+        activities.reposts_count,
+        activities.likes_count,
+        activities.reply_to_activity_uuid,
+        activities.expires_at,
+        activities.created_at
+      FROM public.activities
+      LEFT JOIN public.users ON users.uuid = activities.user_uuid
+      ORDER BY activities.created_at DESC
+      """)
+      print(sql)
+      with pool.connection() as conn:
+        with conn.cursor() as cur:
+          cur.execute(sql)
+          # this will return a tuple
+          # the first field being the data
+          json = cur.fetchone()
+      return json[0]
 
       now = datetime.now(timezone.utc).astimezone()
       span.set_attribute("app.result_length", len(results))
